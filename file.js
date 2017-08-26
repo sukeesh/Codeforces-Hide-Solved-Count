90d568589fd495ddc10f0895e990f859867de2a8
Sukeesh <vsukeeshbabu@gmail.com>
2017-08-26 23:51:39 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         CodeforcesHideSolvedCount
+// @namespace    http://codeforces.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://codeforces.com/problemset
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+   var x = document.getElementsByTagName("a");
+   var y = "<img style=\"vertical-align:middle\"src=\"http://st.codeforces.com/s/49688/images/icons/user.png\">&nbsp\;You can Solve!";
+   var i;
+    for ( i = 0 ; i < x.length ; i ++ ){
+        console.log(x[i]);
+        if(x[i].title == "Participants solved the problem"){
+            x[i].innerHTML = y;
+        }
+    }
+})();
